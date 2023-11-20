2d65440f6205cc57d5b6a9fdcfd92b1d9d4b2cbb
Danial <danialdark12098@gmail.com>
2023-11-20 09:33:27 +0330

    refresh bourse

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const headersGet = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
     'Connection': 'keep-alive',
-    'Cookie': 'cookiesession1=678B28F25E2958B5FB0D2879D40E15CA; ClientId=a1bd46a3-60ce-4dfb-85d8-4842f999ecf1; a1bd46a3-60ce-4dfb-85d8-4842f999ecf1-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJjdXN0b21lciIsInN1YiI6IjIwMDAxMzA3NiIsImp0aSI6IjZhNjFlN2Q4LTY0YjYtNGY2My1hMzNmLWJkYjc0MGNhNmIzNyIsImN1c3RvbWVyLWlkIjoiMjAwMDEzMDc2IiwidXNlci1pZCI6IjU3IiwibmF0aW9uYWwtaWQiOiIyMjk4NjU3ODY2IiwiZXhwIjoxNzAwNDIwMDk4LCJpc3MiOiJTYWhyYUFUSSIsImF1ZCI6IlNhaHJhQVRJIn0.KUHmFIH_FEDjch9V2I4zct2yd1QTcZfANu2Z50v9Dc4',
+    'Cookie': 'cookiesession1=678B28F25E2958B5FB0D2879D40E15CA; ClientId=b704cf93-87d4-4518-a04c-177c9eef736f; b704cf93-87d4-4518-a04c-177c9eef736f-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJjdXN0b21lciIsInN1YiI6IjIwMDAxMzA3NiIsImp0aSI6ImI0Y2QyNWIxLTczZWEtNGFhOS1iNzUxLTllOWFkZTk4MDRkOCIsImN1c3RvbWVyLWlkIjoiMjAwMDEzMDc2IiwidXNlci1pZCI6IjU3IiwibmF0aW9uYWwtaWQiOiIyMjk4NjU3ODY2IiwiZXhwIjoxNzAwNTAzMzI3LCJpc3MiOiJTYWhyYUFUSSIsImF1ZCI6IlNhaHJhQVRJIn0.FDRW602wcqeS34m3u7Mv5pEsQxouYCDuGGkFnsWvcFg',
     'Host': 'sm.exphoenixfuture.ir:8080',
     'Origin': 'https://sm.exphoenixfuture.ir',
     'Referer': 'https://sm.exphoenixfuture.ir/',
@@ -30,7 +30,7 @@ const headersGet = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-    'clientId': 'a1bd46a3-60ce-4dfb-85d8-4842f999ecf1',
+    'clientId': 'b704cf93-87d4-4518-a04c-177c9eef736f',
     'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"'
@@ -180,7 +180,7 @@ async function fetchDataForSymbolConfigs() {
                 }
             }
         }
-        // shouldSendData = true;
+        shouldSendData = true;
 
 
 

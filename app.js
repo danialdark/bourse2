f79c33df3dbc649fa0d29d4395cd3611e392711c
Danial <danialdark12098@gmail.com>
2023-12-02 14:37:18 +0330

    testing

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const headersGet = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'en-US,en;q=0.9',
     'Connection': 'keep-alive',
-    'Cookie': 'cookiesession1=678B28F25E2958B5FB0D2879D40E15CA; ClientId=a7596943-3750-4546-a832-14d7b0a8416f; a7596943-3750-4546-a832-14d7b0a8416f-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJjdXN0b21lciIsInN1YiI6IjIwMDAxMzA3NiIsImp0aSI6IjI2MzI2ZjMwLWRlY2ItNDNjYy1hMGI1LWE3NWJiZmUyZWUwMiIsImN1c3RvbWVyLWlkIjoiMjAwMDEzMDc2IiwidXNlci1pZCI6IjU3IiwibmF0aW9uYWwtaWQiOiIyMjk4NjU3ODY2IiwiZXhwIjoxNzAxNTQwMTU5LCJpc3MiOiJTYWhyYUFUSSIsImF1ZCI6IlNhaHJhQVRJIn0.XQ5cD2sS-s0DrZNe8UDk_QLLqNpzo4kQTbXqvbDEIps',
+    'Cookie': 'cookiesession1=678B28F25E2958B5FB0D2879D40E15CA; a7596943-3750-4546-a832-14d7b0a8416f-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJjdXN0b21lciIsInN1YiI6IjIwMDAxMzA3NiIsImp0aSI6IjI2MzI2ZjMwLWRlY2ItNDNjYy1hMGI1LWE3NWJiZmUyZWUwMiIsImN1c3RvbWVyLWlkIjoiMjAwMDEzMDc2IiwidXNlci1pZCI6IjU3IiwibmF0aW9uYWwtaWQiOiIyMjk4NjU3ODY2IiwiZXhwIjoxNzAxNTQwMTU5LCJpc3MiOiJTYWhyYUFUSSIsImF1ZCI6IlNhaHJhQVRJIn0.XQ5cD2sS-s0DrZNe8UDk_QLLqNpzo4kQTbXqvbDEIps; ClientId=34fa06fe-64e1-4e0c-8b80-741eea0ea2af; 34fa06fe-64e1-4e0c-8b80-741eea0ea2af-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJjdXN0b21lciIsInN1YiI6IjIwMDAxMzA3NiIsImp0aSI6IjI0ZjNiYjc0LTE3OWItNDExZi05ZDYxLWVlMGQ1MzU0ZTAxNSIsImN1c3RvbWVyLWlkIjoiMjAwMDEzMDc2IiwidXNlci1pZCI6IjU3IiwibmF0aW9uYWwtaWQiOiIyMjk4NjU3ODY2IiwiZXhwIjoxNzAxNTU4MzU3LCJpc3MiOiJTYWhyYUFUSSIsImF1ZCI6IlNhaHJhQVRJIn0.6WIJKj3iyV84ypl2u7LmQ6WNgJ5yYbKBVcIDWZcwWRk',
     'Host': 'sm.exphoenixfuture.ir:8080',
     'Origin': 'https://sm.exphoenixfuture.ir',
     'Referer': 'https://sm.exphoenixfuture.ir/',
@@ -30,7 +30,7 @@ const headersGet = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-    'clientId': 'a7596943-3750-4546-a832-14d7b0a8416f',
+    'clientId': '34fa06fe-64e1-4e0c-8b80-741eea0ea2af',
     'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"'
